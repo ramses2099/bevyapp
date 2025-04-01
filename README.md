50ad317fb35bfefb11f3af2df8c1e95906f4ecff
ramses2099 <ramses2099@gmail.com>
2025-04-01 12:21:39 -0400

    latest bevy app

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # bevyapp
+
+# BEVY
+- use Entity Component System paradigm.
+
+- Components: Rust structs that implement the Component trait
+```
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+```
+
+- Systems: normal Rust functions
+```
+fn print_position_system(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {} {}", position.x, position.y);
+    }
+}
+
+```
+
+- Entities: a simple type containing a unique 
+```
+struct Entity(u64);
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use bevy::prelude::*;
+
+//
+#[derive(Component)]
+struct Person;
+#[derive(Component)]
+struct Name(String);
+
+// Component
+
+// System
+fn hello_world_system() {
+    println!("Hello World!");
+}
+//
+fn startup_system(mut commands: Commands) {
+    commands.spawn((Person, Name("Alice".to_string())));
+    commands.spawn((Person, Name("Bob".to_string())));
+    commands.spawn((Person, Name("Charlie".to_string())));
+}
+
+fn greeting_people_system(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hello, {}!", name.0);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, startup_system)
+        .add_systems(Update, (hello_world_system, greeting_people_system))
+        .run();
 }
